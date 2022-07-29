c147db1d52441d0221fda05f7306d4e2f0f137ce
JamesB <James.buchmann@gmail.com>
2022-07-29 08:29:03 -0500

    all the if conditionals tested and work

--- index.js
+++ index.js
@@ -2,55 +2,84 @@
 // const path = require('path');
 // const mysql = require('mysql2');
 const inquirer = require("inquirer");
-
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "whatdo",
-      message: "What do you want to do?",
-      choices: [
-        "view all employees",
-        "add employee",
-        "update employee Role",
-        " view all roles",
-        "add role",
-        "view all departments",
-        "add department",
-        "exit"
-      ],
-    },
-  ])
-  .then((answers) => {
-    if (answers.choices == "view all employees") {
-      viewEmployee();
-    }
-    if (answers.choices == "add employee") {
-      addEmployee();
-    }
-    if (answers.choice == 'update employee Role') {
+function questionloop() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "whatdo",
+        message: "What do you want to do?",
+        choices: [
+          "view all employees",
+          "add employee",
+          "update employee Role",
+          "view all roles",
+          "add role",
+          "view all departments",
+          "add department",
+          "exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      if (answers.whatdo == "view all employees") {
+        viewEmployee();
+      }
+      if (answers.whatdo == "add employee") {
+        addEmployee();
+      }
+      if (answers.whatdo == "update employee Role") {
         updateRole();
-    }
-    if ( answers.choice == 'view all roles') {
+      }
+      if (answers.whatdo == "view all roles") {
         viewRoles();
-    }
-    if (answers.choice == 'view all departments') {
+      }
+      if (answers.whatdo == "add role") {
+        addRole();
+      }
+      if (answers.whatdo == "view all departments") {
         viewDepartments();
-    }
-    if ( answers.choice == 'add department') {
+      }
+      if (answers.whatdo == "add department") {
         addDepartment();
-    } 
-    else {}
-  });
+      }
+      if (answers.whatdo == "exit") {
+        exit()
+      }
+    });
+}
 
-  // VIEW EMPLOYEE -----------------
+// VIEW EMPLOYEE -----------------
+function viewEmployee() {
+  console.log("view employed");
+}
 
-  // ADD EMPLOYEE ----------------------
+// ADD EMPLOYEE ----------------------
+function addEmployee() {
+  console.log("add employee");
+}
+// UPDATE ROLE --==================
+function updateRole() {
+  console.log("update role");
+}
+// VIEW ROLE ========================
+function viewRoles() {
+  console.log(" view role");
+}
+// ADD ROLE ====================
+function addRole() {
+  console.log("add role");
+}
 
-  // UPDATE ROLE --==================
-
-  // VIEW ROLE ========================
-
-  //VEW DEPARTMENTS ======================
-
-  //ADD DEPARTMENT =================
+//VEW DEPARTMENTS ======================
+function viewDepartments() {
+  console.log("view department");
+}
+//ADD DEPARTMENT =================
+function addDepartment() {
+  console.log("add department");
+}
+//EXIT =========================
+function exit(){
+    console.log('this will do something eventually')
+}
